7a11cc223211d2440ddce45a785a7a6d2a721da9
Philip Obosi <philip.c.obosi@gmail.com>
2023-12-23 02:29:57 +0100

    feat: init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NothingToSee = () => {
+    return (
+        <div>
+            <Link href="/intro">Documentation</Link>
+        </div>
+    )
+}
+
+export default NothingToSee
